b4d427a34d8af177ddd32f508a88d2864ce44f0e
floripabalada <floripa.balada@gmail.com>
2025-09-20 10:03:32 -0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/sv3_app_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.totem_barberia_vip.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/totem_barberia_vip/barbeariavip_totem/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.totem_barberia_vip.barbeariavip_totem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
